a9acb6c483644dfd61b2a71f45f61f2ba24441ac
pablo <pablo@pablos-MBP-M2.lan>
2024-04-29 17:13:05 +0100

    Implementing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css"; // Import CSS file for styling
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section">
+          <h3>Connect with me:</h3>
+          <ul>
+            <li>
+              <a
+                href="https://wa.me/message/OZNQX54WO67AO1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-whatsapp"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.criativodevs.online/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-globe"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/criativodevs"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-square"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Contact:</h3>
+          <p>Mobile: +351 911 509 368</p>
+          <p>Email: criativo.devs@criativodevs.online</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
