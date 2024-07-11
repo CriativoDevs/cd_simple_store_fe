734731dd3b76fcc89a8059d6c64cdefb06f2a370
pablo <pablo@pablos-MBP-M2.lan>
2024-07-11 15:22:08 +0100

    Small changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import Rating from "./Rating";
+// import Rating from "./Rating";
 import { Link } from "react-router-dom";
 import { addToCart } from "../actions/cartActions";
 import { useNavigate } from "react-router-dom";
